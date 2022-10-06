fc58bd9ac992bfb9ba221af5169aa7618dce3918
pedro-git-projects <pedro.generic.email@gmail.com>
2022-10-06 14:40:00 -0300

    Rectangle QVector

--- Qarea/main.cpp
+++ Qarea/main.cpp
@@ -1,13 +1,20 @@
 #include <QTextStream>
+#include <QVector>
 #include "rectangle.h"
 
 int main() {
     QTextStream cout(stdout);
+    QVector<Rectangle*> rectangles;
 
-    Rectangle r1;
-    Rectangle r2(5.0, 2.0);
+    rectangles.push_back(new Rectangle());
+    rectangles.push_back(new Rectangle(7.5, 10.5));
+    rectangles.push_back(new Rectangle(5.0, 2.0));
 
-    cout << r2 << Qt::endl;
+    for(int i = 0; i < rectangles.size(); i++) {
+        cout << "Rectangle " << i + 1 << Qt::endl;
+        cout << *rectangles[i] << Qt::endl;
+        cout << "----------------------------" << Qt::endl;
+    }
 
     cout.flush();
     return 0;
