0f55173d981143d9aa811897b2acd89b6e2fdd5c
pedro-git-projects <pedro.generic.email@gmail.com>
2022-10-07 13:40:20 -0300

    outputting areas to file

--- Qarea/main.cpp
+++ Qarea/main.cpp
@@ -1,52 +1,26 @@
 #include <QTextStream>
+#include <iostream>
 #include <QVector>
 #include <QFile>
-#include "rectangle.h"
-#include "rectangles.h"
 #include "app.h"
-
-bool isRunning = true;
 
 int main() {
     QFile outputFile("areas.txt");
     outputFile.open(QIODevice::WriteOnly | QIODevice::Text);
-
-    QTextStream cout(stdout);
-
-
     QTextStream out(&outputFile);
 
     App* app = new App();
+    app->loop();
 
-   app->loop();
-  // cout << app->list;
-   app->list.printAreas();
+    auto areasVec = app->list.getAreas();
 
-
-    /*
-    QTextStream cin(stdin);
-
-    cout << "Enter '0' to quit" << Qt::endl;
-    float w, l;
-    while(isRunning) {
-        cout << "-> Please enter the rectangle width and length separated by space" << Qt::endl;
-        cin >> w >> l;
-        //if(w == 0 || l == 0) isRunning = false;
-        if(!(cin.status() == QTextStream::Ok)) {
-            cout.setStatus(QTextStream::Ok);
-            cout << "-> Please enter float values!" << Qt::endl;
-            cin >> w >> l;
-        }
+    for(int i = 0; i < areasVec.size(); i++) {
+        std::cout << "Outputing to file..." << std::endl;
+        out << areasVec[i] << Qt::endl;
     }
 
-
-    Rectangle* r1 = new Rectangle(4.0, 2.0);
-    Rectangles* rs = new Rectangles({r1, new Rectangle(1.0, 2.0), new Rectangle(7.0, 4.0)});
-
-    cout << *rs;
-
-    cout.flush();
-    */
+    outputFile.flush();
     outputFile.close();
+
     return 0;
 }
